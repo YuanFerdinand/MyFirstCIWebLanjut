24fbc8e76b6c06bc46699ddd989f10d4e19bc99b
YuanFerdinand <lionelkailuhu@gmail.com>
2020-10-16 16:54:26 +0700

    new update

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -8,5 +8,12 @@ class Home extends BaseController
 	}
 
 	//--------------------------------------------------------------------
+	public function show(){
 
+		$data['nama'] = 'Yuan';
+		$data['jurusan'] = 'Ilkomp';
+		echo view('mahasiswa/header',$data);
+		echo view('mahasiswa/index',$data);
+		echo view('mahasiswa/footer',$data);
+	}
 }
